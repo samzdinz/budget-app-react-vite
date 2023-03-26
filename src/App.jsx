d139d0866572a67ccde4beb920931add2b3c63ae
samzdinz <samzdinz@gmail.com>
2023-03-27 05:22:43 +0700

    add routes and helpers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//route
+import Dashboard, { dashboardLoader } from './pages/dashboard';
+import Error from './pages/Error';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+    },
+]);
+
 function App() {
     return (
         <div className='App'>
-            <h2>Hai Keyzie</h2>
+            <RouterProvider router={router} />
         </div>
     );
 }
